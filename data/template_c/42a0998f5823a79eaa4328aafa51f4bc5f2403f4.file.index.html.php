ef103008c77bd9f3972765d6032bb17737f43ebb
gusheng <804657288@qq.com>
2017-09-26 13:55:50 +0800

    后添加的

---
+++ data/template_c/42a0998f5823a79eaa4328aafa51f4bc5f2403f4.file.index.html.php
@@ -0,0 +1,71 @@
+<?php /* Smarty version Smarty-3.1.16, created on 2017-09-25 09:01:09
+         compiled from "tpl\index.html" */ ?>
+<?php /*%%SmartyHeaderCode:646759c8c17d45a4d3-11675354%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '42a0998f5823a79eaa4328aafa51f4bc5f2403f4' => 
+    array (
+      0 => 'tpl\\index.html',
+      1 => 1506330063,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '646759c8c17d45a4d3-11675354',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.16',
+  'unifunc' => 'content_59c8c17d483416_70163692',
+  'variables' => 
+  array (
+    'data' => 0,
+    'value' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_59c8c17d483416_70163692')) {function content_59c8c17d483416_70163692($_smarty_tpl) {?><!doctype html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title></title>
+	<script src="img/js/jquery-1.5.2.min.js" type="text/javascript"></script>
+
+
+</head>
+
+
+<body>
+<table class="tablesorter" cellspacing="0" style="margin:0"> 
+	<thead> 
+		<tr>  
+   				<th>标题</th>
+   				<th>作者</th>
+		</tr> 
+	</thead>
+	<tbody>
+	<?php  $_smarty_tpl->tpl_vars['value'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['value']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['data']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['value']->key => $_smarty_tpl->tpl_vars['value']->value) {
+$_smarty_tpl->tpl_vars['value']->_loop = true;
+?>
+		<tr>
+			<td><a href="index.php?controller=index&method=articlecontent&id=<?php echo $_smarty_tpl->tpl_vars['value']->value['id'];?>
+"><?php echo $_smarty_tpl->tpl_vars['value']->value['title'];?>
+</a></td> 
+			<td><?php echo $_smarty_tpl->tpl_vars['value']->value['author'];?>
+</td> 
+   		</tr>
+	<?php }
+if (!$_smarty_tpl->tpl_vars['value']->_loop) {
+?>
+		<tr>
+			<td  colspan=3>
+				暂无新闻
+			</td>
+		</tr>
+	<?php } ?>
+	</tbody>
+</table>
+</body>
+</html><?php }} ?>
---
+++ data/template_c/911893e306fdd38a9b7bb56fbe1e5882645f07d8.file.articlecontent.html.php
@@ -0,0 +1,45 @@
+<?php /* Smarty version Smarty-3.1.16, created on 2017-09-25 09:15:53
+         compiled from "tpl\articlecontent.html" */ ?>
+<?php /*%%SmartyHeaderCode:2200859c8c8787cad63-79666686%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '911893e306fdd38a9b7bb56fbe1e5882645f07d8' => 
+    array (
+      0 => 'tpl\\articlecontent.html',
+      1 => 1506330950,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2200859c8c8787cad63-79666686',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.16',
+  'unifunc' => 'content_59c8c878805084_67487147',
+  'variables' => 
+  array (
+    'data' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_59c8c878805084_67487147')) {function content_59c8c878805084_67487147($_smarty_tpl) {?><!doctype html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title></title>
+	<script src="img/js/jquery-1.5.2.min.js" type="text/javascript"></script>
+</head>
+<body>
+<a href="index.php">返回主页</a>
+<div style="width:600px;margin:10px auto;">
+	<h1><?php echo $_smarty_tpl->tpl_vars['data']->value['title'];?>
+</h1>
+	<p><span style="margin-right:20px;">作者：<?php echo $_smarty_tpl->tpl_vars['data']->value['author'];?>
+</span><span>来源：<?php echo $_smarty_tpl->tpl_vars['data']->value['from'];?>
+</span></p>
+	<div><?php echo $_smarty_tpl->tpl_vars['data']->value['content'];?>
+</div>
+</div>
+</body>
+</html><?php }} ?>
